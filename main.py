6ae62383f251567c398298f2413d0c4132f58cd9
Zachary Sweet <ztsc97@mst.edu>
2017-09-25 19:11:47 -0500

    Finished the Assignment

--- main.py
+++ main.py
@@ -3,47 +3,54 @@ from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-caesar_form = """
+caesar_form ="""
 
 <!DOCTYPE html>
 <html>
     <head>
         <style>
-            form{
+            form{{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea{
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
+            
         </style>
     </head>
     <body>
     <!-- create your form here -->
-    <form action = "/hello" methods = ['POST']> 
-    <label>Rotate by:<input type = "text" name = "rot"></label>
-    <br></br>
-    <input type = "textarea" name = "text">
-    <input type = "submit">
-    </form>
-    </body>
-<html>
-"""
-@app.route("/",methods = ['POST'])
-def encrypt():
+     <form method ='POST'> 
+        <label for = "rot_k">Rotate by:</label>
+        <input name = "rot" id = "rot_k" type = "text" value = 0 />
+        <br>
 
-    return "hello"
+        <textarea name = "text">{final_text}</textarea>
+        
+        <input type = "submit" value = "submit"/>
+     </form>
+    </body>
+  </html>
+"""
+@app.route("/", methods = ['POST'])
+def encrypt():
+        
+   rotnum = int(request.form['rot'])
+   textmess = request.form['text']
+   new_text = rotate_string(textmess,rotnum)
+   return caesar_form.format(final_text = new_text)
+
+
 
 @app.route("/")
 def index():
-
-    
-    return caesar_form
+    return caesar_form.format(final_text = '')
 
 app.run()
